bfa7db818925690c9446c98b4075e538fa464e23
gercius <gerciuswork@gmail.com>
2024-10-13 15:23:17 +0300

    update main script

--- excel-js.js
+++ excel-js.js
@@ -1,6 +1,5 @@
 const ExcelJS = require("exceljs");
 
-// const excelFilePath = "data-no-completion-checks.xlsx";
 const excelFilePath = "data.xlsx";
 
 const workbook = new ExcelJS.Workbook();
@@ -8,101 +7,96 @@ const quests = [];
 
 let quest = {
     location: "",
-    quest: "",
-    details: [],
+    questInfo: {
+        name: "",
+        hyperlink: "",
+        type: "",
+        isCompleted: false,
+    },
+    extraDetails: [
+        {
+            description: "",
+            hyperlink: "",
+            isCompleted: false,
+        },
+    ],
 };
 
 workbook.xlsx.readFile(excelFilePath).then(() => {
-    const worksheet = workbook.getWorksheet(2); // Access the 2nd sheet
+    const worksheet = workbook.getWorksheet(2);
     console.log("Sheet name:", worksheet.name);
 
     const rows = worksheet.getRows(2, worksheet.rowCount);
 
     for (let i = 0; i < rows.length; i++) {
-        console.log(i);
         const row = rows[i];
-        const location = row.getCell(1).value; // Assuming Location is in column 1
-        // console.log("value", row.getCell(2).value.text);
-        // console.log(row.getCell(2).fill);
-        const name = row.getCell(2).value; // Assuming Quest is in column 2
-        const details = row.getCell(4).value; // Assuming Quest Details are in column 3
 
-        if (row.getCell(2).value.text === "Kaer Morhen (1)") {
-            console.log(row.getCell(2).value);
-            console.log(row.getCell(2).fill);
-            break;
-        }
+        const location = row.getCell(1).value;
+        console.log(location);
 
-        if (!quest && details) {
-            quest.details.push(details);
-            return;
-        }
+        const questInfo = row.getCell(2).value;
+        const questCompleted = row.getCell(3).value;
+        const extraDetails = row.getCell(4).value;
+        const extraDetailsCompleted = row.getCell(6).value;
+
+        // break;
+
+        // const name = questInfo.text;
+        // const hyperlink = questInfo.hyperlink;
+        // const questCellFillValue = questInfo.fill.bgColor.argb;
+        // const questType = getQuestType(questCellFillValue);
+
+        // console.log(questType);
+
+        // break;
+
+        // const quest = row.getCell(2).value.text;
+
+        // if (!quest && details) {
+        //     quest.details.push(details);
+        //     continue;
+        // }
 
         // If there is a new quest, push the previous quest and start a new one
-        if (quest.quest) {
-            quests.push(quest);
-        }
+        // if (quest.quest) {
+        //     quests.push(quest);
+        // }
 
-        quest = {
-            location: location || "",
-            quest: name || "",
-            details: [],
-        };
+        // quest = {
+        //     location: location || "",
+        //     quest: name || "",
+        //     details: [],
+        // };
 
-        if (details) {
-            quest.details.push(details);
-        }
+        // if (details) {
+        //     quest.details.push(details);
+        // }
 
         // console.log(quest);
 
         // break;
-        // if (quests.length > 2) break;
+        // if (quests.length > 40) break;
     }
-
-    // Push the last quest if it's valid
-    if (quest.quest) {
-        quests.push(quest);
-    }
-
-    // console.log(quests);
-
-    // worksheet.eachRow((row, rowNumber) => {
-    //     if (rowNumber === 1) return; // Skip header row
-
-    //     const location = row.getCell(1).value; // Assuming Location is in column 1
-    //     const name = row.getCell(2).value; // Assuming Quest is in column 2
-    //     const details = row.getCell(3).value; // Assuming Quest Details are in column 3
-
-    //     if (!quest && details) {
-    //         quest.details.push(details);
-    //         return;
-    //     }
-
-    //     // If there is a new quest, push the previous quest and start a new one
-    //     if (quest.quest) {
-    //         quests.push(quest);
-    //     }
-
-    //     quest = {
-    //         location: location || "",
-    //         quest: name || "",
-    //         details: [],
-    //     };
-
-    //     if (details) {
-    //         quest.details.push(details);
-    //     }
-
-    //     if (quests.length > 10) {
-    //         console.log(quests);
-    //         return;
-    //     }
-    // });
-
-    // return;
-
-    // Push the last quest
-    // if (quest.location) {
-    // quests.push(quest);
-    // }
 });
+
+function toCapitalCase(str) {
+    str = str.toLowerCase().split(" ");
+    str = str.map((word) => word.slice(0, 1).toUpperCase() + word.slice(1));
+    return str.join(" ");
+}
+
+function getQuestType(cellFillValue) {
+    console.log(cellFillValue);
+
+    const questTypes = {
+        FFE06666: "Main Quest",
+        FFF6B26B: "Side Quest",
+        FFFFD966: "Contract",
+        FF93C47D: "Treasure Hunt",
+        FFA4C2F4: "Gwent & The Heroes' Pursuit",
+        FF3D85C6: "Scavenger Hunt",
+        FF8E7CC3: "Chance Encounter",
+    };
+
+    return questTypes[cellFillValue];
+}
