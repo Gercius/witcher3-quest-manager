4b02dd9b6846bd246b98ad0408513c62041aab11
gercius <gerciuswork@gmail.com>
2024-10-13 15:21:43 +0300

    add quest schema example, quest type - quest cell color mapping

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 https://www.reddit.com/r/Witcher3/comments/18ut9sj/witcher_3_optimal_quest_order_guide_updated/
 
 https://docs.google.com/spreadsheets/d/12H5lZC6vLFj0JToLSD2ZE7EcMN6MrmltL9SM_G6EUNU/edit?usp=sharing
+
+Colors bgColor
+
+main quest - FFE06666
+side quest - FFF6B26B
+contract - FFFFD966
+treasure hunt - FF93C47D
+gwent - FFA4C2F4
+scavenger hunt - FF3D85C6
+chance encounter - FF8E7CC3
--- test.js
+++ test.js
@@ -1,9 +1,15 @@
-let quest = {
-    location: "",
-    quest: "",
-    details: [],
-};
+// const mainQuest = "Kaer Morhen (1)";
+// const sideQuest = "A Frying Pan, Spick and Span (1)";
+// const contract = "Devil by the Well (2)";
+// const treasureHunt = "Deserter Gold (3)";
+// const gwent = "Gwent: Collect 'em All! (1)";
+// const scavengerHunt = "Viper Silver Sword (Basic) (6)";
+// const chance = "At the Mercy of Strangers";
 
-quest.details.push("kekw");
-
-console.log(quest);
+// if (quest.includes(chance)) {
+//     console.log("found");
+//     console.log(quest);
+//     console.log(row.getCell(2).value);
+//     console.log(row.getCell(2).fill);
+//     break;
+// }
