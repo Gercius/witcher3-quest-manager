d2acc7acc08a209a1653191304fbc32f69c32ca6
gercius <gerciuswork@gmail.com>
2024-10-14 12:49:14 +0300

    finish (hopefully) quest data extraction script

--- excel-js.js
+++ excel-js.js
@@ -1,4 +1,5 @@
 const ExcelJS = require("exceljs");
+const fs = require("node:fs");
 
 const excelFilePath = "data.xlsx";
 
@@ -7,103 +8,142 @@ const quests = [];
 let quest = null;
 let currentQuestName = null;
 
-// let quest = {
-//     location: "",
-//     questInfo: {
-//         name: "",
-//         hyperlink: "",
-//         type: "",
-//         isCompleted: false,
-//     },
-//     extraDetails: [],
-// };
-
 workbook.xlsx.readFile(excelFilePath).then(() => {
     const worksheet = workbook.getWorksheet(2);
     console.log("Sheet name:", worksheet.name);
 
     const rows = worksheet.getRows(2, worksheet.rowCount);
 
+    // let shitQuests = [];
+    // for (let i = 0; i < rows.length; i++) {
+    //     const row = rows[i];
+
+    //     const locationRaw = row.getCell(1).value;
+    //     const questInfo = row.getCell(2);
+    //     const extraDetails = row.getCell(4).value;
+
+    // if (i === 456) {
+    // if (!locationRaw) {
+    //     console.log(i);
+    //     console.log(questInfo.value);
+    //     console.log(locationRaw);
+    //     break;
+    // }
+    // break;
+    // }
+
     for (let i = 0; i < rows.length; i++) {
-        console.log("quests.length", quests.length);
+        try {
+            const row = rows[i];
 
-        const row = rows[i];
-        const locationRaw = row.getCell(1).value;
-        const questInfo = row.getCell(2);
-        const extraDetails = row.getCell(4).value;
+            // Push last quest
+            if (!row.getCell(2).value) {
+                quests.push(quest);
+                break;
+            }
 
-        const questName = questInfo.value.text;
-        const questHyperlink = questInfo.value.hyperlink;
-        const questCellFillValue = questInfo.fill?.bgColor.argb;
-        const questType = questCellFillValue ? getQuestType(questCellFillValue) : "";
+            const locationRaw = row.getCell(1).value;
+            const questInfo = row.getCell(2);
+            const extraDetails = row.getCell(4).value;
 
-        if (currentQuestName && currentQuestName !== questName) {
-            // Push the completed quest to the quests array
-            quests.push(quest);
+            const questName = questInfo.value.text;
+            const questHyperlink = questInfo.value.hyperlink;
+            const questCellFillValue = questInfo.fill?.bgColor.argb;
+            const questType = questCellFillValue ? getQuestType(questCellFillValue) : "";
 
-            // Start a new quest
-            quest = {
-                location: toCapitalCase(locationRaw),
-                questInfo: {
-                    name: questName,
-                    hyperlink: questHyperlink,
-                    type: questType,
+            if (currentQuestName && currentQuestName !== questName) {
+                quests.push(quest);
+
+                quest = {
+                    location: capitalizeLocationStr(locationRaw),
+                    questInfo: {
+                        name: questName,
+                        hyperlink: questHyperlink,
+                        type: questType,
+                        isCompleted: false,
+                    },
+                    extraDetails: [],
+                };
+            }
+
+            if (!quest) {
+                quest = {
+                    location: capitalizeLocationStr(locationRaw, questName),
+                    questInfo: {
+                        name: questName,
+                        hyperlink: questHyperlink,
+                        type: questType,
+                        isCompleted: false,
+                    },
+                    extraDetails: [],
+                };
+                currentQuestName = questName;
+            }
+
+            if (extraDetails) {
+                const extraDetailsDescription = handleExtraDetailsRichText(extraDetails);
+                const extraDetailsHyperlink = extraDetails.hyperlink || "";
+
+                quest.extraDetails.push({
+                    description: extraDetailsDescription,
+                    hyperlink: extraDetailsHyperlink,
                     isCompleted: false,
-                },
-                extraDetails: [],
-            };
-        }
+                });
+            }
 
-        if (!quest) {
-            quest = {
-                location: toCapitalCase(locationRaw),
-                questInfo: {
-                    name: questName,
-                    hyperlink: questHyperlink,
-                    type: questType,
-                    isCompleted: false,
-                },
-                extraDetails: [],
-            };
             currentQuestName = questName;
+        } catch (error) {
+            console.error(error, "\n At row:", i);
         }
-
-        if (extraDetails) {
-            const extraDetailsDescription = extraDetails.text || extraDetails;
-            const extraDetailsHyperlink = extraDetails.hyperlink || "";
-
-            quest.extraDetails.push({
-                description: extraDetailsDescription,
-                hyperlink: extraDetailsHyperlink,
-                isCompleted: false,
-            });
-        }
-
-        if (i === rows.length - 1) {
-            quests.push(quest);
-        }
-
-        if (quests.length >= 1) {
-            const kekw = JSON.stringify(quests[0], undefined, 2);
-            console.log(kekw);
-
-            break;
-        }
-
-        currentQuestName = questName;
     }
+
+    fs.writeFile("quests.json", JSON.stringify(quests, null, 2), (err) => {
+        if (err) {
+            console.error("Error writing to file", err);
+        } else {
+            console.log("quests.json has been saved successfully.");
+        }
+    });
+
+    console.log(quests.length);
 });
 
-function toCapitalCase(str) {
-    str = str.toLowerCase().split(" ");
-    str = str.map((word) => word.slice(0, 1).toUpperCase() + word.slice(1));
-    return str.join(" ");
+function capitalizeLocationStr(string) {
+    try {
+        const exceptions = ["and", "of"];
+        let str = string.includes("/") ? string.toLowerCase().split("/") : string.toLowerCase().split(" ");
+
+        str = str.map((word) => {
+            if (exceptions.includes(word)) {
+                return word;
+            }
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        });
+
+        return string.includes("/") ? str.join("/") : str.join(" ");
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function handleExtraDetailsRichText(extraDetails) {
+    let extraDetailsDescription;
+
+    if (extraDetails?.text?.richText) {
+        extraDetailsDescription = extraDetails.text.richText.text;
+    } else if (extraDetails?.text) {
+        extraDetailsDescription = extraDetails.text;
+    } else {
+        extraDetailsDescription = extraDetails || "";
+    }
+
+    return extraDetailsDescription;
 }
 
 function getQuestType(cellFillValue) {
     if (!cellFillValue) return "";
 
-    const questTypes = {
+    const questTypesMap = {
         FFE06666: "Main Quest",
         FFF6B26B: "Side Quest",
         FFFFD966: "Contract",
@@ -113,5 +153,5 @@ function getQuestType(cellFillValue) {
         FF8E7CC3: "Chance Encounter",
     };
 
-    return questTypes[cellFillValue];
+    return questTypesMap[cellFillValue];
 }
