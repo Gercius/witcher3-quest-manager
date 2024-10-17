dd3abbdfb18e4abe82ba3a27c89c4cec69e2063a
gercius <gerciuswork@gmail.com>
2024-10-17 21:59:28 +0300

    implement initial theme switching

---
+++ src/main.js
@@ -0,0 +1,15 @@
+const body = document.querySelector("body");
+const toggleThemeButton = document.querySelector(".theme-switch input");
+
+// Set OS prefered theme on load
+window.onload = () => {
+    const preferDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    preferDarkTheme ? body.classList.add("dark-theme") : "";
+    preferDarkTheme ? (toggleThemeButton.checked = true) : (toggleThemeButton.checked = false);
+};
+
+toggleThemeButton.addEventListener("click", toggleTheme);
+
+function toggleTheme() {
+    toggleThemeButton.checked ? body.classList.add("dark-theme") : body.classList.remove("dark-theme");
+}
