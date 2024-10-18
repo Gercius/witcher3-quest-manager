f516dd9a0613e6a43231ee330d60def87c253867
gercius <gerciuswork@gmail.com>
2024-10-18 14:07:38 +0300

    add the capability to save user's theme preference

--- src/themes.js
+++ src/themes.js
@@ -1,15 +1,29 @@
 (function handleThemes() {
     const body = document.querySelector("body");
     const toggleThemeButton = document.querySelector(".theme-switch input");
+    const themeStorageKey = "preferred-theme";
 
     // Set OS prefered theme on load
+    const storedTheme = localStorage.getItem(themeStorageKey);
     const preferDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    preferDarkTheme ? body.classList.add("dark-theme") : "";
-    preferDarkTheme ? (toggleThemeButton.checked = true) : (toggleThemeButton.checked = false);
+
+    if (storedTheme) {
+        storedTheme === "dark" ? body.classList.add("dark-theme") : body.classList.remove("dark-theme");
+        toggleThemeButton.checked = storedTheme === "dark";
+    } else {
+        preferDarkTheme ? body.classList.add("dark-theme") : "";
+        toggleThemeButton.checked = preferDarkTheme;
+    }
 
     toggleThemeButton.addEventListener("click", toggleTheme);
 
     function toggleTheme() {
-        toggleThemeButton.checked ? body.classList.add("dark-theme") : body.classList.remove("dark-theme");
+        if (toggleThemeButton.checked) {
+            body.classList.add("dark-theme");
+            localStorage.setItem(themeStorageKey, "dark");
+        } else {
+            body.classList.remove("dark-theme");
+            localStorage.setItem(themeStorageKey, "light");
+        }
     }
 })();
