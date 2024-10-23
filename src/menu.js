268abdf4a5631212bfbe015af4d4d36199e948ed
gercius <gerciuswork@gmail.com>
2024-10-23 21:02:35 +0300

    make menu open from the side

--- src/menu.js
+++ src/menu.js
@@ -5,21 +5,11 @@ const menu = document.querySelector(".menu");
 menuButton.addEventListener("click", handleMenu);
 
 function handleMenu() {
-    const isMenuClosed = menu.classList.contains("hidden");
-
-    if (isMenuClosed) {
-        menu.classList.remove("hidden");
-        menu.classList.add("menu-open");
-    } else {
-        menu.classList.add("hidden");
-        menu.classList.remove("menu-open");
-    }
-
     menuButtonImages.forEach((img) => {
         img.classList.contains("hidden") ? img.classList.remove("hidden") : img.classList.add("hidden");
     });
 
-    // console.log(isMenuClosed);
+    const width = menu.style.width;
 
-    // menu.classList.remove("hidden");
+    width === "0px" || !width ? (menu.style.width = "250px") : (menu.style.width = "0px");
 }
