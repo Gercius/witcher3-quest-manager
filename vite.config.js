eb66a5a33ad0101d6ab8ef1f07bd1241344161dd
gercius <gerciuswork@gmail.com>
2024-11-27 12:40:16 +0200

    Minify HTML

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
-export default {
+import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+
+export default defineConfig({
     build: {
         outDir: "docs",
     },
-};
+    plugins: [
+        createHtmlPlugin({
+            minify: true,
+        }),
+    ],
+});
