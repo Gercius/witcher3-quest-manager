a0bd7a61850bae0fb6be2b6c809b5fb2f886e472
gercius <gerciuswork@gmail.com>
2024-12-03 15:34:01 +0200

    Fix typo

--- src/ts/quest-handling/render.ts
+++ src/ts/quest-handling/render.ts
@@ -4,11 +4,11 @@ export function renderCompletedPercentage() {
     const questsData = manageState.get();
     const totalQuests = questsData.length;
     const totalCompleted = questsData.filter((quest) => quest.isCompleted === true).length;
-    const percentege = ((totalCompleted / totalQuests) * 100).toFixed(1);
-    const completedHeaderEl = document.querySelector(".completed-percentege");
+    const percentage = ((totalCompleted / totalQuests) * 100).toFixed(1);
+    const completedHeaderEl = document.querySelector(".completed-percentage");
     if (!completedHeaderEl) {
         console.error("Completed percentage showing element not found!");
         return;
     }
-    completedHeaderEl.textContent = percentege;
+    completedHeaderEl.textContent = percentage;
 }
