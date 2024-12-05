dd2e19989fe0bdcc666a5b70680666eeedec107b
gercius <gerciuswork@gmail.com>
2024-12-05 09:02:42 +0200

    Show completed quest count

--- src/ts/quest-handling/render.ts
+++ src/ts/quest-handling/render.ts
@@ -1,14 +1,18 @@
 import { manageState } from "./manageState";
 
 export function renderCompletedPercentage() {
+    const questCountEl = document.querySelector(".quest-count");
+    const completedHeaderEl = document.querySelector(".completed-percentage");
+    if (!completedHeaderEl || !questCountEl) {
+        console.error("completedHeaderEl or questCountEl not found!");
+        return;
+    }
+
     const questsData = manageState.get();
     const totalQuests = questsData.length;
     const totalCompleted = questsData.filter((quest) => quest.isCompleted === true).length;
     const percentage = ((totalCompleted / totalQuests) * 100).toFixed(1);
-    const completedHeaderEl = document.querySelector(".completed-percentage");
-    if (!completedHeaderEl) {
-        console.error("Completed percentage showing element not found!");
-        return;
-    }
+
     completedHeaderEl.textContent = percentage;
+    questCountEl.innerHTML = `${totalCompleted}/${totalQuests} quests`;
 }
