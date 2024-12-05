bbb25c201e9a85b9976224c17c44fe1566097410
gercius <gerciuswork@gmail.com>
2024-12-05 14:42:32 +0200

    Move images to imgs folder

--- src/ts/populateDOM.ts
+++ src/ts/populateDOM.ts
@@ -33,7 +33,7 @@ export async function populateQuestTable() {
         // Add marker for quests whose order doesn't matter
         if (!quest.questInfo.orderMatters) {
             const questOrderNoMatterImg = document.createElement("img");
-            questOrderNoMatterImg.src = "public/assets/kekw.jpg";
+            questOrderNoMatterImg.src = "imgs/kekw.jpg";
             nameTd.appendChild(questOrderNoMatterImg);
         }
 
